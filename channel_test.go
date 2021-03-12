e5ff047199b8930e18a8c585566d29d925ec41b3
nanandt <rizkyfatih007@gmail.com>
2021-03-13 00:00:16 +0700

    create channel

---
+++ channel_test.go
@@ -0,0 +1,28 @@
+package golang_goroutine
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestCreateChannel(t *testing.T) {
+	channel := make(chan string) // channel yg menerima tipe data string
+	//channel <- "Rizky" // mengirim data ke channel
+	//data := <- channel // mengambil data ke channel
+	//fmt.Println(<- channel) // mengambil data channel dan dimasukan ke parameter
+	defer close(channel) // selesai membuat channel direkomendasikan menutup channel nya
+
+	go func() {
+		time.Sleep(2 *time.Second)
+		channel <- "Muhamad Fatikhurrizky"
+		fmt.Println("Selesai mengirim data ke channel")
+	}()
+
+	data := <-channel //mengambil data dari channel
+	fmt.Println(data)
+	time.Sleep(5 *time.Second)
+
+
+}
+
