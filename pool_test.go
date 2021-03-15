3874b54b12b8de2f56bf01ccbcf00b945e1a2253
nanandt <rizkyfatih007@gmail.com>
2021-03-15 22:08:45 +0700

    sync.Pool

---
+++ pool_test.go
@@ -0,0 +1,32 @@
+package golang_goroutine
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestPool(t *testing.T) {
+	pool := sync.Pool{
+		New: func() interface{} {
+			return "New"
+		},
+	}
+
+	pool.Put("Muhamad")
+	pool.Put("Fatih")
+	pool.Put("Rizky")
+
+	for i := 0; i < 10; i++ {
+		go func() {
+			data := pool.Get()
+			fmt.Println(data)
+			pool.Put(data)
+		}()
+	}
+
+	time.Sleep(3 *time.Second)
+	fmt.Println("Selesai")
+}
+
